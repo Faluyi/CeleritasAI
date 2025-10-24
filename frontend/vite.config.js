706e1ff6dc9abc19181d7dc7f804706c2ec91c1d
Faluyi Isaiah <phirmzhy@gmail.com>
2025-10-24 23:24:03 +0100

    repo clean up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
   server: {
-    port: 3000,
+    port: 3002,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
